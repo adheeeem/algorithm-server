f311f7d3dab82d4a30802053d802e95b2be0d69d
Azimjon Fayzulloev <sherman.peabody99@gmail.com>
2024-10-11 00:12:41 +0500

    setup ports with kestrel

--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -4,7 +4,6 @@ using Application;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-Console.WriteLine("Hakuna matata");
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -12,6 +11,15 @@ builder.Configuration.AddEnvironmentVariables();
 builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddApplicationServices();
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+	options.ListenAnyIP(5220);
+	options.ListenAnyIP(7117, listenOptions =>
+	{
+		listenOptions.UseHttps();
+	});
+});
 
 var app = builder.Build();
 
