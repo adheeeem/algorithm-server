bc3c474ee7b8c7de8b886a9468aeb6c5283b9f26
Azimjon Fayzulloev <sherman.peabody99@gmail.com>
2024-10-10 23:03:25 +0500

    added workflow file and updated http port

--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Configuration.AddEnvironmentVariables();
 builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddApplicationServices();
