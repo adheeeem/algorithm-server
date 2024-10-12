411ac4c5caa161da310154d867a89deb3e06d66d
Azimjon Fayzulloev <sherman.peabody99@gmail.com>
2024-10-12 10:01:45 +0500

    some script changes

--- scripts/run_migrations_server.sh
+++ scripts/run_migrations_server.sh
@@ -2,7 +2,7 @@ dbHost=$DB_HOST
 dbPassword=$DB_PASSWORD
 
 export GOOSE_DRIVER=postgres
-export GOOSE_DBSTRING="host=${dbHost} port=5432 user=virtualmenu password=${dbPassword} dbname=virtualmenu"
+export GOOSE_DBSTRING="host=${dbHost} port=5432 user=algorithm password=${dbPassword} dbname=algorithm"
 
 cd migrations
 goose up
