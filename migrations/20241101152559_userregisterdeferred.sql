6e81e339658449680a26ab67046d4b967dae02fb
Azimjon Fayzulloev <sherman.peabody99@gmail.com>
2024-11-01 21:18:52 +0500

    change migration syntax error

--- migrations/20241101152559_userregisterdeferred.sql
+++ migrations/20241101152559_userregisterdeferred.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
 alter table app_user_enrollment drop constraint app_user_enrollment_app_user_id_fkey;
-alter table app_user_enrollment add constraint app_user_enrollment_app_user_id_fkey foreign key (app_user_id) references app_user(id) deferred initially deferred;
+alter table app_user_enrollment add constraint app_user_enrollment_app_user_id_fkey foreign key (app_user_id) references app_user(id) deferrable initially deferred;
 -- +goose StatementEnd
 
 -- +goose Down
